528e01899f23c49f6895987218c96b3ebf96f7d7
gewalttat <rbhorses@mail.ru>
2022-08-07 14:27:05 +0500

    init

--- src/components/EmptyState/EmptyState.tsx
+++ src/components/EmptyState/EmptyState.tsx
@@ -1,6 +1,6 @@
-import { Box, Button } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Box, Button } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const EmptyState = () => {
     return (
